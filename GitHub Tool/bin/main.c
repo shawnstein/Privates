20fa83ee725b0d5107eb3ec5b4cb5c4804b88881
shawnstein <sherman@viacobra.de>
2014-05-09 18:03:47 +0200

    projekterstellung

---
+++ GitHub Tool/bin/main.c
@@ -0,0 +1,15 @@
+/*
+git add * && git commit -m "Kommentar" && git push origin master
+cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && gdb ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+
+	return 0;
+}
