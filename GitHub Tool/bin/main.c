ec635ec18b1925eed2e0b785a494690a73a92dc6
shawnstein <sherman@viacobra.de>
2014-05-09 18:08:56 +0200

    kommentare gesetzt

--- GitHub Tool/bin/main.c
+++ GitHub Tool/bin/main.c
@@ -1,7 +1,7 @@
 /*
 git add * && git commit -m "Kommentar" && git push origin master
-cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && gdb ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
-cc /home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/main.c -g -std=c99 && ./home/sandor/Dokumente/GitHub/HTWK/ADS_Prog1/a.out
+cc /home/sandor/Dokumente/GitHub/Privates/GitHub\ Tool/bin/main.c -g -std=c99 && gdb ./home/sandor/Dokumente/GitHub/Privates/GitHub\ Tool/src/a.out
+cc /home/sandor/Dokumente/GitHub/Privates/GitHub\ Tool/bin/main.c -g -std=c99 && ./home/sandor/Dokumente/GitHub/Privates/GitHub\ Tool/src/a.out
 */
 
 #include <stdio.h>
