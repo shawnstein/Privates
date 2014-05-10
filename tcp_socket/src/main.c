86403755049b8e822df10c6c4076b054a11ce936
shawnstein <sherman@viacobra.de>
2014-05-10 19:15:12 +0200

    tcp_socket angelegt

---
+++ tcp_socket/src/main.c
@@ -0,0 +1,15 @@
+/*
+git add * && git commit -m "Kommentar" && git push origin master
+cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && ./GitHub\ Tool//src/a.out
+cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && gdb ./GitHub\ Tool//src/a.out
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	return 0;
+}
