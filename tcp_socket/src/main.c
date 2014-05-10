cf222090695dbc46eac9c98888b8c37867929737
shawnstein <sherman@viacobra.de>
2014-05-10 19:17:51 +0200

    tcp_socket angelegt

--- tcp_socket/src/main.c
+++ tcp_socket/src/main.c
@@ -8,6 +8,10 @@ cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && gdb ./GitHub
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int socket(int family, int type, int protocol);
 
 int main() {
 
