b913f5bc7c6b7026ae458846772edae65b0a6ddc
shawnstein <Sandor@Sandor-Notebook>
2014-05-11 15:57:49 +0200

    adding name

--- tcp_socket/src/main.c
+++ tcp_socket/src/main.c
@@ -1,4 +1,7 @@
 /*
+tcp_socket by shawnstein
+
+
 git add * && git commit -m "Kommentar" && git push origin master
 cc tcp_socket/src/main.c -std=c99 -g -o tcp_socket/bin/a.out && ./tcp_socket/bin/a.out
 cc tcp_socket/src/main.c -std=c99 -g -o tcp_socket/bin/a.out && gdb ./tcp_socket/bin/a.out
