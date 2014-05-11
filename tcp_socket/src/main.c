044d4140c4f18f598251ff2e82fd24ceb6dd5818
shawnstein <sherman@viacobra.de>
2014-05-11 02:18:40 +0200

    socket_addr

--- tcp_socket/src/main.c
+++ tcp_socket/src/main.c
@@ -1,7 +1,7 @@
 /*
 git add * && git commit -m "Kommentar" && git push origin master
-cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && ./GitHub\ Tool//src/a.out
-cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && gdb ./GitHub\ Tool//src/a.out
+cc tcp_socket/src/main.c -std=c99 -g -o tcp_socket/bin/a.out && ./tcp_socket/bin/a.out
+cc tcp_socket/src/main.c -std=c99 -g -o tcp_socket/bin/a.out && gdb ./tcp_socket/bin/a.out
 
 */
 
@@ -10,10 +10,25 @@ cc GitHub\ Tool/bin/main.c -std=c99 -g -o GitHub\ Tool/src/a.out && gdb ./GitHub
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+struct sockaddr_in {
+	uint8_t 		sin_len;
+	sa_family_t		sin_family;
+	in_port_t		sin_port;
+	struct in_addr	sin_addr;
+	char			sin_zero[];
+}
+
+struct in_addr {
+	in_addr_t 		s_addr;
+}
 
 int connect(int sockfd, struct sockaddr *address, socklen_t adress_length);
 
 int main() {
+	struct sockaddr__in addr;
+	memset(&addr, 0, sizeof(addr));
 
 	return 0;
 }
