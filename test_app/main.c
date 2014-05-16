eb74b1d65b10f0190efd14312df0a0802ddb6136
Sandor <sandor@vUbuntu-Srv.fritz.box>
2014-05-17 01:41:31 +0200

    Test projekt

---
+++ test_app/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Sandor
+ *
+ * Created on 17. Mai 2014, 01:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
